d26d4c288a69318162fb5c1bbafb619e992e0c2f
LizetteO <55018147+LizetteO@users.noreply.github.com>
2022-03-18 14:43:05 +0200

    Initialize LizetteO-patch-1

---
+++ android/app/src/main/java/com/trust/web3/demo/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.trust.web3.demo
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val DAPP_URL = "https://js-eth-sign.surge.sh"
+        private const val CHAIN_ID = 56
+        private const val RPC_URL = "https://bsc-dataseed2.binance.org"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val provderJs = loadProviderJs()
+        val initJs = loadInitJs(
+            CHAIN_ID,
+            RPC_URL
+        )
+        WebView.setWebContentsDebuggingEnabled(true)
+        val webview: WebView = findViewById(R.id.webview)
+        webview.settings.run {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+        WebAppInterface(this, webview, DAPP_URL).run {
+            webview.addJavascriptInterface(this, "_tw_")
+
+            val webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    view?.evaluateJavascript(provderJs, null)
+                    view?.evaluateJavascript(initJs, null)
+                }
+            }
+            webview.webViewClient = webViewClient
+            webview.loadUrl(DAPP_URL)
+        }
+    }
+
+    private fun loadProviderJs(): String {
+        return resources.openRawResource(R.raw.trust_min).bufferedReader().use { it.readText() }
+    }
+
+    private fun loadInitJs(chainId: Int, rpcUrl: String): String {
+        val source = """
+        (function() {
+            var config = {
+                chainId: $chainId,
+                rpcUrl: "$rpcUrl",
+                isDebug: true
+            };
+            window.ethereum = new trustwallet.Provider(config);
+            window.web3 = new trustwallet.Web3(window.ethereum);
+            trustwallet.postMessage = (json) => {
+                window._tw_.postMessage(JSON.stringify(json));
+            }
+        })();
+        """
+        return  source
+    }
+}
